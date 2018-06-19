d8c214dbe74e5feaa98cdef7f1997996d340fed7
eduardolc2 <edu_10agro@hotmail.com>
2018-06-19 17:40:30 -0300

    Pastas do curso de Angular

---
+++ hello-typescript/main.ts
@@ -0,0 +1,28 @@
+// JS
+var minhaVar = 'minha variavel';
+
+
+function minhaFunc (x, y) {
+    return x + y;
+}
+
+
+//ES 6 ou ES 2015
+let numb = 2;
+const pi = 3.14;
+
+var numeros = [1, 2, 3];
+
+//js
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+//ES 6
+numeros.map(valor => valor * 2);
+
+class Matematica {
+    soma(x, y) {
+        return x + y;
+    }
+}
